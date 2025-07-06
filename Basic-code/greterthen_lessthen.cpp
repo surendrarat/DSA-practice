d5f2a63d4188d2a183b9065ee65dd19e445488bc
surendar rathor <143333444+surendrarat@users.noreply.github.com>
2025-07-06 06:35:05 +0000

    greterthen and less then of two number

---
+++ Basic-code/greterthen_lessthen.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+    int a;
+    cout<<"enter the value of a"<<endl;
+    cin>>a;
+    int b;
+    cout<<"enter the value of b"<<endl;
+    cin>>b;
+    if(a<b){
+        cout<<"b is greter then a " <<b<<" > "<<a<<endl;
+    }
+   else {
+        cout<<"a is greter then b "<<a<<" > "<<b<<endl;
+    }
+}
