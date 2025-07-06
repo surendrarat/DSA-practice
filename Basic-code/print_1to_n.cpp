0ce5e24bcdfc9fecc5712090015092f0737bc7f8
surendar rathor <143333444+surendrarat@users.noreply.github.com>
2025-07-06 07:02:05 +0000

    print from 1 to n number

---
+++ Basic-code/print_1to_n.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int N;
+    cout<<"enter the N number ";
+    cin>>N;
+    int i=1;
+    while (i<=N)
+    {
+        cout<<i<<endl;
+        i++;
+    }
+    
+}
