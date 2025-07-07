4fc585f1237155a800d036f9d119fc0ca9be85bc
surendar rathor <143333444+surendrarat@users.noreply.github.com>
2025-07-07 14:35:47 +0000

    for variable , datatype ,  typecasting and operator

---
+++ variable datatype basic/typecasting.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main (){
+// implicit typecasting
+// small to big
+
+int a='b';
+int g=43;
+double e=g;
+cout<<sizeof(e)<<endl;
+cout<<e<<endl;
+cout<<a<<endl;
+int size=sizeof(a);
+cout<<size<<endl;
+
+
+// explicity type casting
+char my=59;
+// or
+char go =int (111);
+cout<<go<<endl;
+cout<<my;
+
+
+// operator -- relation , logical and arithematic operator
+/*
+
+< > <= >= !=
+&& || !
++ - * /
+*/
+}
