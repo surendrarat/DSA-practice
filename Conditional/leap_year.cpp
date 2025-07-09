23bfe00fdc9ad1d339193dfbaa8f70dfd746b469
surendar rathor <143333444+surendrarat@users.noreply.github.com>
2025-07-09 19:54:09 +0000

    check the year is leap year or not

---
+++ Conditional/leap_year.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+      int year;
+    cout<<"enter the year"<<endl;
+    cin>>year;
+    if (year%4==0)
+          {
+            cout<<"this year is leap year"<<endl;
+            
+          }
+          else
+          {
+            cout<<"this year is not leap year\n";
+          }
+          
+    return 0;
+}
+
