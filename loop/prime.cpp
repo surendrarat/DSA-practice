1bf0f17e6c65e09c04bcb8bffe554abffa8e6ce7
surendar rathor <143333444+surendrarat@users.noreply.github.com>
2025-07-10 19:01:57 +0000

    this is incomplete in this time

--- loop/prime.cpp
+++ loop/prime.cpp
@@ -9,13 +9,14 @@ int main(){
     int i=2;
     while (i<n)
     {
-       if (n%i==0)
+       if(n%i!=0) {
+        cout<<"the number is not prime :"<<i<<endl;
+       }
+      else  if (n%1==0&&n%n==0)
        {
-        cout<<"the number is not  prime :";
+        cout<<"the number is   prime :"<<i<<endl;
        }
-       else {
-        cout<<"the number is prime :";
-       }
+       
        i++;
        
     }
