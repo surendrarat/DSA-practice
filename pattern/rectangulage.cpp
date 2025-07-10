c751313db2c8fe002a1dffea6c8c09d5e98f7230
surendar rathor <143333444+surendrarat@users.noreply.github.com>
2025-07-10 19:26:37 +0000

    this patter by using the while loop

--- pattern/rectangulage.cpp
+++ pattern/rectangulage.cpp
@@ -7,15 +7,30 @@ int main(){
     cout<<"enter the rows :"<<endl;
     cin>>n;
      
-    for (int i = 1; i <= n; i++)
-    {
-       for (int j = 1; j <= n; j++)
-       {
-        cout<<" * ";
-       }
-       cout<<endl;
+    // for (int i = 1; i <= n; i++)
+    // {
+    //    for (int j = 1; j <= n; j++)
+    //    {
+    //     cout<<" * ";
+    //    }
+    //    cout<<endl;
        
+    // }
+
+    // using the while loop
+    int i=1;
+    while (i<=n)
+    {
+        int j=1;
+        while (j<=n)
+        {
+            cout<<" * ";
+            j++;
+        }
+        cout<<endl;
+        i++;
     }
+    
     
     
 }
