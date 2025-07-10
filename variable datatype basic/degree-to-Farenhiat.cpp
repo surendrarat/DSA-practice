a68a81b114ea0b3c445d409ad736e2706aeebf6a
surendar rathor <143333444+surendrarat@users.noreply.github.com>
2025-07-10 18:11:14 +0000

    change the vlaue to degree to farenhiat

---
+++ variable datatype basic/degree-to-Farenhiat.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+    float dgree;
+    cout<<"enter the dgree :"<<endl;
+    cin>>dgree;
+    
+    float farenhiat;
+
+     farenhiat=dgree*1.8+32;
+    cout<<"the vlaue of farenhiat is :"<<farenhiat<<endl;
+}
