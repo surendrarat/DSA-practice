1419c0b34ba7f1131fca435a74f8bd494c18d9b3
surendar rathor <143333444+surendrarat@users.noreply.github.com>
2025-07-10 17:00:24 +0000

    find out the vlaue of input,space, and enter

---
+++ Conditional/input-space-tage-enter-vlaue.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a ;
+    // this is provide the space , enter , tage value or get these value for display or show.
+
+    // a=cin.get();
+    // cout<<"the vlaue of enter"<<a<<endl;
+
+    // a=cin.get();
+    // cout<<"the vlaue of tage "<<a<<endl;
+// and important
+    a=cin.get();
+    cout<<"the vlaue of space "<<a<<endl;
+    return 0;
+
+}
